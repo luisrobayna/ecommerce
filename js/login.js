bbe0ac5e7f22f200da95a162497756e8f1e1e91f
luisrobayna <luismiguelrobayna@hotmail.com>
2021-08-19 21:18:17 -0300

    Probando login google

--- js/login.js
+++ js/login.js
@@ -18,5 +18,23 @@ document.addEventListener("DOMContentLoaded", function(e){
         }else{
             localStorage.setItem('logeado','true');
         }
-    })  
+    })
+    
+    function onSignIn(googleUser) {
+        // Useful data for your client-side scripts:
+        var profile = googleUser.getBasicProfile();
+        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log("Image URL: " + profile.getImageUrl());
+        console.log("Email: " + profile.getEmail());
+  
+        // The ID token you need to pass to your backend:
+        var id_token = googleUser.getAuthResponse().id_token;
+        console.log("ID Token: " + id_token);
+        return id_token;
+    }
+    let id = onSignIn()
+    console.log(id);
 });
